7abd9030ef890c48a650f0a1f367584bc7ab7c62
Joseph Peters <petersjl@rose-hulman.edu>
2023-10-06 13:03:58 -0400

    install wslu if in wsl

--- LinuxLoader.fish
+++ LinuxLoader.fish
@@ -1,3 +1,7 @@
 if [ -d ./LinuxFiles -a -f ./LinuxFiles/config.fish ]
     cp ./LinuxFiles/config.fish ~/.config/fish/config.fish
 end
+
+if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ]
+    dpkg -l wslu &> /dev/null || apt-get install -y wslu
+end
