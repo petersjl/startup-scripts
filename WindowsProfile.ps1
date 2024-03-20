f3696574cd05a140074cf127cac488908d6a6a3b
Joseph Peters <petersjl@rose-hulman.edu>
2024-03-20 12:29:08 -0400

    Add functions to powershell

--- WindowsProfile.ps1
+++ WindowsProfile.ps1
@@ -1,4 +1,34 @@
 new-alias -Name np -Value notepad
+# General functions
+Function prof { np $PROFILE }
 Function w {wsl ~}
-Function c {code .}
 Function e {explorer .}
+Function c {
+    param ($filepath)
+    if ($null -eq $filepath) {
+        code .
+    }
+    else {
+        code $filepath
+    }
+}
+
+# Git
+Function gph { git push }
+Function gpl { git pull }
+Function gf { git fetch }
+
+Function hide-conf {
+	git update-index --skip-worktree #relative path to file to ignore
+}
+
+Function show-conf {
+	git update-index --no-skip-worktree #relative path to file to ignore
+}
+
+# Docker
+<#
+Function dockerstop { docker stop $(docker ps -a -q) }
+Function dcud { docker compose up -d}
+Function dcubd {docker compose up --build -d}
+#>
