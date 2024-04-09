2e5a142e5712abb73ee0f13f9d3b5ab5badccd5a
Joseph Peters <petersjl@rose-hulman.edu>
2024-04-09 16:57:17 -0400

    Update Windows Loader to handle modules

--- WindowsLoader.ps1
+++ WindowsLoader.ps1
@@ -1,2 +1,19 @@
-New-Item $PROFILE -ItemType File -Force 1> $null
-Copy-Item .\WindowsProfile.ps1 $PROFILE 1> $null
+$profile_location = ".\WindowsFiles\profile.ps1"
+$modules_location = ".\WindowsFiles\Modules"
+
+# Install the profile
+if(Test-Path $profile_location -PathType Leaf){
+    New-Item $PROFILE -ItemType File -Force 1> $null
+    Copy-Item $profile_location $PROFILE 1> $null
+}
+else {
+    Write-Output "Could not find $profile_location"
+}
+
+# Install any modules in ./WindowsFiles/Modules
+if(Test-Path $modules_location -PathType Container) {
+    $module_destination = $env:PSModulePath.Split(";")[0]
+    New-Item $module_destination -ItemType Directory -Force 1> $null
+    Copy-Item -Path "$modules_location\*" -Destination $module_destination -Recurse
+    Unblock-File -Path "$module_destination\*"
+}
