17384928e4e31630c1716d717cd70b9d4d25a9c5
Joseph Peters <jlpeters@sep.com>
2024-04-10 16:36:18 -0400

    Conditionally install fish

--- LinuxLoader.sh
+++ LinuxLoader.sh
@@ -3,9 +3,11 @@
 #make sure apt-get is installed
 command -v apt-get > /dev/null || { echo "apt-get is required to run this script."; exit 1; }
 
-sudo apt-add-repository -y ppa:fish-shell/release-3
-sudo apt update
-sudo apt install -y fish
+if ! command -v fish > /dev/null; then
+    sudo apt-add-repository -y ppa:fish-shell/release-3
+    sudo apt update
+    sudo apt install -y fish
+fi
 chsh -s /usr/bin/fish
 
 if test -f ./LinuxLoader.fish; then
