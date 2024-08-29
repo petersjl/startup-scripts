1620d77c1d7397141aa3f4d64589ced8dfee4a55
Joseph Peters <petersjl@rose-hulman.edu>
2024-08-29 14:19:11 -0400

    Update windows profile

--- WindowsFiles/profile.ps1
+++ WindowsFiles/profile.ps1
@@ -1,6 +1,11 @@
 new-alias -Name np -Value notepad
-# General functions
+# --General functions
 Function prof { code $PROFILE }
+function refreshpath {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
+                ";" +
+                [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 Function w {wsl ~}
 Function e {explorer .}
 Function c {
@@ -13,14 +18,61 @@ Function c {
     }
 }
 
-# Git
-Function gph { git push }
-Function gpl { git pull }
-Function gf { git fetch }
+# --Git
+Function gph { git push $Args }
+Function gpl { git pull $Args }
+Function gf { git fetch $Args }
+Function gs { git status $Args }
+Function gas { git add * $Args }
+Function gcm { git commit -m $Args }
 
-# Docker
-<#
+# --Docker
+$defaultProfile = 'local'
 Function dockerstop { docker stop $(docker ps -a -q) }
-Function dcud { docker compose up -d}
-Function dcubd {docker compose up --build -d}
-#>
+Function dcud {
+    param(
+        [parameter(position = 0)] [string] $profile = $defaultProfile,
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    docker compose --profile $profile up -d
+}
+Function dcub {
+    param(
+        [parameter(position = 0)] [string] $profile = $defaultProfile,
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    docker compose --profile $profile up --build
+}
+Function dcubd {
+    param(
+        [parameter(position = 0)] [string] $profile = $defaultProfile,
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    docker compose --profile $profile up --build -d
+}
+Function dcubw {
+    param(
+        [parameter(position = 0)] [string] $profile = $defaultProfile,
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    docker compose --profile $profile up --build --watch
+}
+Function dcdv {
+    param(
+        [parameter(position = 0)] [string] $profile = $defaultProfile,
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    docker compose --profile $profile down --volumes @Remaining 
+}
+# Connect to postgress. Replace variables before use
+# Function dpsql {docker exec -e PGPASSWORD=<password> -it $(docker ps -q) psql -U <database-user> -d <database-name>}
+
+# --Python
+Function pm { python -m $Args }
+Function pt { 
+    param(
+        [string] $filepath = '.',
+        [parameter(position = 1, ValueFromRemainingArguments=$true)] $Remaining
+    )
+    python -m pytest $filepath $Remaining
+}
