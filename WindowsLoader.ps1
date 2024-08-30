73b2a8443e82bf224b3caaac6f447ae369f8f207
Joseph Peters <petersjl@rose-hulman.edu>
2024-08-30 16:38:39 -0400

    Allow mode selection from loader

--- WindowsLoader.ps1
+++ WindowsLoader.ps1
@@ -1,5 +1,9 @@
+param(
+    [Parameter(position=0)][string]$Mode
+)
+
 # install new powershell
 winget install --id Microsoft.Powershell --source winget
 
 # run install script in new powershell
-powershell -Command "& 'C:\Program Files\PowerShell\7\pwsh.exe' .\WindowsFiles\install.ps1"
+powershell -Command "& 'C:\Program Files\PowerShell\7\pwsh.exe' .\WindowsFiles\install.ps1 $Mode"
