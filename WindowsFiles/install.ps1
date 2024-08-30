121c4af7634f55ed71805e59cc6ee924a1f82dca
Joseph Peters <petersjl@rose-hulman.edu>
2024-08-30 15:50:15 -0400

    Allow mode from windows loader

--- WindowsFiles/install.ps1
+++ WindowsFiles/install.ps1
@@ -1,3 +1,7 @@
+param(
+    [Parameter(position=0)][string]$Mode
+)
+
 # Make this script run from it's own directory
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
@@ -5,6 +9,31 @@ Push-Location $dir
 
 $profile_location = ".\profile.ps1"
 $modules_location = ".\Modules"
+
+$mode_home = "home"
+$mode_work = "work"
+
+Function Assign-Mode{
+    param([Parameter(Position=0)][string] $mode)
+    if(($null -eq $mode) -or ($mode -eq "")){
+        return $null}
+    if(($mode -eq "work") -or ($mode -eq "w")){
+        return $mode_work
+    }
+    elseif(($mode -eq "home") -or ($mode -eq "h")){
+        return $mode_home
+    }
+    else{
+        & {Write-Host "[$mode] is not a valid Mode"}
+        return $null
+    }
+}
+
+$installMode = Assign-Mode $Mode
+while($null -eq $installMode){
+    $Mode = Read-Host -Prompt "Is this for work or home"
+    $installMode = Assign-Mode $Mode
+}
 
 # Install the profile
 if(Test-Path $profile_location -PathType Leaf){
@@ -25,3 +54,26 @@ if(Test-Path $modules_location -PathType Container) {
 
 # Run other install packages
 pwsh ./VSCode/install.ps1
+
+# Install other winget modules
+$wingetPackages = @(
+    # For all machines
+    "Microsoft.VisualStudioCode",
+    "Mozilla.Firefox"
+)
+
+if($installMode -eq $mode_home){
+    $wingetPackages += @(
+        # On personal machines only
+        "Discord.Discord",
+        "Valve.Steam"
+    )
+}
+elseif($installMode -eq $mode_work){
+    $wingetPackages += @(
+        # On work machines only
+    )
+}
+foreach($package in $wingetPackages){
+    winget install -e --id $package --accept-source-agreements --accept-package-agreements
+}
